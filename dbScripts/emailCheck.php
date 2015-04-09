90e0651d9e56563ee7f76adf5b7ace5d9b9073fb
Marcus <Marcusli029@gmail.com>
2015-04-09 19:30:55 -0400

    php to MySql scripts

---
+++ dbScripts/emailCheck.php
@@ -0,0 +1,28 @@
+<?php
+	include 'credentials.php';
+
+	 // Create connection
+    $conn = new mysqli($address, $username, $password, $database);
+	$sql = "SELECT UserEmail FROM user where UserEmail =  '".$_GET['param1']."'";
+	$result = NULL;
+    // Check connection	
+	 if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+	    $result = $conn->query($sql);
+		if (!$result) {
+		    throw new Exception("Database Error [{$this->database->errno}] {$this->database->error}");
+		} else {
+		    $array = array();	
+			   while($row = $result->fetch_assoc()) $array[] = $row;
+			echo $result->num_rows;//	Echo result in JSON
+		
+		}
+		
+		$conn->close();
+    }
+
+	
+	//check if username exists in the database
+
+?>
