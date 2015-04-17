a3a83ca09493fd8d56c4e280cefd0d9a9935b650
GorkyMing <erming.gao@uconn.edu>
2015-04-17 15:30:29 -0400

    Main page file added again with more php files.

---
+++ Main Page/Update Resume.php
@@ -0,0 +1,40 @@
+ <!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Update Resume</title>
+<meta name="keywords" content="Update resume">
+<link href="stylesheet_main.css" rel="stylesheet" type="text/css" >
+</head>
+
+<body>
+<header>
+<div class="topbar">
+  <div class="navbutton">
+    <nav>
+         	<a class="HomePage" href="Setting.html">Setting</a>
+            <a>Log out</a>
+  
+    </nav>
+  </div>
+  <!-- navbutton --> 
+</div>
+<!-- topbar -->
+
+<div class="sidebar">
+  <h1>Update Resume</h1>
+  <div class="navbutton">
+    <nav>
+      <a class="HomePage" href="Search.html">Search</a>
+      <a class="HomePage" href="Manage.html">Manage Applications</a>
+
+  
+    </nav>
+  </div>
+  <!-- navbutton --> 
+</div>
+<!-- sidebar -->
+</header>
+
+</body>
+</html>
