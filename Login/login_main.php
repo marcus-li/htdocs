3bf5536c1fdcd9922de8da8345b501f13cefb51e
Marcus <Marcusli029@gmail.com>
2015-04-18 21:02:37 -0400

    change redirection to main page if user already logged in

--- Login/login_main.php
+++ Login/login_main.php
@@ -2,7 +2,7 @@
 include('login.php'); // Includes Login Script
 
 if(isset($_SESSION['login_user'])){
-header("location: profile.php");
+header("Location: ../MainPage/main_page.php");
 }
 ?>
 <!DOCTYPE html>
