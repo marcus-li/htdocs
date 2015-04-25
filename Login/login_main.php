4428834c59e00b26c96f6699ed79b30b28d6d90e
GorkyMing <erming.gao@uconn.edu>
2015-04-25 13:48:55 -0400

    Added poster_Registration

--- Login/login_main.php
+++ Login/login_main.php
@@ -13,19 +13,19 @@ header("Location: ../MainPage/main_page.php");
 </head>
 <body>
 <div id="main">
-<h1>UConn Job Search Login</h1>
-<div id="login">
-<h2>Login Form</h2>
-<form action="" method="post">
-<label>UserName :</label>
-<input id="UserName" name="UserName" placeholder="username" type="text">
-<label>Password :</label>
-<input id="UserPassword" name="UserPassword" placeholder="**********" type="password">
-<b id="Register"><a href="../Registration/register.php">Not registered yet? Register</a></b>
-<input name="submit" type="submit" value=" Login ">
-<span><?php echo $error; ?></span>
-</form>
-</div>
+  <h1>UConn Job Search Login</h1>
+  <div id="login">
+    <h2>Login Form</h2>
+    <form action="" method="post">
+      <label>UserName :</label>
+      <input id="UserName" name="UserName" placeholder="username" type="text">
+      <label>Password :</label>
+      <input id="UserPassword" name="UserPassword" placeholder="**********" type="password">
+      <b id="Register"><a href="../Registration/register.php">Not registered yet? Register</a></b>
+      <input name="submit" type="submit" value=" Login ">
+      <span><?php echo $error; ?></span>
+    </form>
+  </div>
 </div>
 </body>
 </html>
