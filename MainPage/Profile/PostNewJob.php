25605433a00d5b6012cb0de650b54500fcfb3c6a
axcel12 <axcel.duarte_espinoza@uconn.edu>
2015-04-25 17:50:50 -0400

    PostNewJobs update

--- MainPage/Profile/PostNewJob.php
+++ MainPage/Profile/PostNewJob.php
@@ -55,14 +55,16 @@ if(!isset($_SESSION['login_user'])){
 <div class = "content">
 
 <h3 class = "contentHeader">Post New Jobs:</h3>
-	<div style = "padding-left: 80px">Display jobs previously posted. Exclusive option for Posters.</div>
+<div style='padding-left:50px; padding-right:100px;'>
+Create and Post new jobs. Exclusive option for Posters.
+</div><br><hr><br>
 
-<h3 class = "contentHeader">Post New Job:</h3>
+<h3 class = "contentHeader">Company Information:</h3>
 
-<!-- post a new job-->
+<!-- Company Info-->
 
 <div style='padding-left:50px; padding-right:100px;'>
-<form method ='post' action='submitPaymentMethod.php'>
+<form method ='post'>
             
     <?php
 	include '../../dbscripts/credentials.php';
@@ -86,18 +88,50 @@ if(!isset($_SESSION['login_user'])){
             
 			<table border='0'><tr>
             
-			<td>CompanyID: </td><td><input name="CompanyId" id="CompanyId" value="<?php echo $info["CompanyId"];?>" readonly="readonly"/></td></tr>
-			<td>CompanyName:</td> <td><input name = 'CompanyName' id="CompanyName" value="<?php echo $info["CompanyName"];?>" readonly="readonly"/></td></tr>		
-			<tr><td>Company Street: </td><td> <input  name = "CompanyStreet" id="CompanyStreet1" value="<?php echo $info["CompanyStreet1"];?>" readonly="readonly"/></td></tr>
-			<tr><td>Company City: </td><td> <input  name = "CompanyCity" id="CompanyCity" value="<?php echo $info["CompanyCity"];?>" readonly="readonly"/></td></tr>
-			<tr><td>Company Email: </td><td> <input  name = "CompanyEmail" id="CompanyEmail" value="<?php echo $info["CompanyEmail"];?>" readonly="readonly"/></td></tr>
-            <tr><td>Company Phone: </td><td> <input  name = "CompanyPhone" id="CompanyPhone" value="<?php echo $info["CompanyPhone"];?>" readonly="readonly"/></td></tr>
-            <tr><td>Company Fax: </td><td> <input  name = "CompanyFax" id="CompanyFax" value="<?php echo $info["CompanyFax"];?>" readonly="readonly"/></td></tr>
-            <tr><td>Company Home Page: </td><td> <input  name = "CompanyHomePage" id="CompanyHomePage" value="<?php echo $info["CompanyHomePage"];?>" readonly="readonly"/></td></tr>
+			<td>CompanyID: </td><td><input name="CompanyId" id="CompanyId" value="<?php echo $info["CompanyId"];?>" readonly/></td></tr>
+			<td>CompanyName:</td> <td><input name = 'CompanyName' id="CompanyName" value="<?php echo $info["CompanyName"];?>" readonly/></td></tr>		
+			<tr><td>Company Street: </td><td> <input  name = "CompanyStreet" id="CompanyStreet1" value="<?php echo $info["CompanyStreet1"];?>" readonly/></td></tr>
+			<tr><td>Company City: </td><td> <input  name = "CompanyCity" id="CompanyCity" value="<?php echo $info["CompanyCity"];?>" readonly/></td></tr>
+			<tr><td>Company Email: </td><td> <input  name = "CompanyEmail" id="CompanyEmail" value="<?php echo $info["CompanyEmail"];?>" readonly/></td></tr>
+            <tr><td>Company Phone: </td><td> <input  name = "CompanyPhone" id="CompanyPhone" value="<?php echo $info["CompanyPhone"];?>" readonly/></td></tr>
+            <tr><td>Company Fax: </td><td> <input  name = "CompanyFax" id="CompanyFax" value="<?php echo $info["CompanyFax"];?>" readonly/></td></tr>
+            <tr><td>Company Home Page: </td><td> <input  name = "CompanyHomePage" id="CompanyHomePage" value="<?php echo $info["CompanyHomePage"];?>" readonly/></td></tr>
+</table></form></div><br><hr><br>	
+
+
+<h3 class = "contentHeader">Post a new Job:</h3>
+<div style='padding-left:50px; padding-right:100px;'>
+
+<!-- Post a new job-->
+
+<form method ='post' action='postNewJob.php'>
+			<table border='0'><tr>
             
-			<tr><td colspan='2' align='center'><br><input  type='submit' name = 'add' value = 'Add'></td></tr>
-			</table></form></div><br><hr><br>	
- 
+			<td>JobId: </td><td><input name="JobId" id="JobId"/></td></tr>
+			<td>Job List Date:</td> <td><input name = 'JobListDate' id="JobListDate"/></td></tr>		
+			<tr><td>Company Name: </td><td> <input  name = "CompanyName" id="CompanyName" value="<?php echo $info["CompanyName"];?>" readonly/></td></tr>
+			<tr><td>Job Title: </td><td> <input  name = "JobTitle" id="JobTitle"/></td></tr>
+			<tr><td>Job City: </td><td> <input  name = "JobCity" id="JobCity" value="<?php echo $info["CompanyCity"];?>"/></td></tr>
+            <tr><td>Job Zip: </td><td> <input  name = "JobZip" id="JobZip"/></td></tr>
+            <tr><td>Job Duties: </td><td> <textarea  name = "JobDuties" id="JobDuties"></textarea></td></tr>
+            <tr><td>Years of Experience: </td><td> <input  name = "JobYRSExperience" id="JobYRSExperience"/></td></tr>
+            <tr><td>Job Degree Types: </td><td> <input  name = "JobDegreeType" id="JobDegreeType"/></td></tr>
+            
+            
+            <tr><td>Job Degree Areas: </td><td> <input  name = "JobDegreeAreas" id="JobDegreeAreas"/></td></tr>
+            
+            
+            <tr><td>Job Low Range: </td><td> <input  name = "JobJLowRange" id="JobJLowRange"/></td></tr>
+            <tr><td>Job High Range: </td><td> <input  name = "JobHighRange" id="JobHighRange"/></td></tr>
+            <tr><td>Job Fill Status: </td><td> <select  name = "JobFillStatus" id="JobFillStatus"><option value="Yes">Yes</option>
+            	<option value="No" selected>No</option>
+            </select></td></tr>
+            
+            <tr><td colspan='2' align='center'><br><input  type='submit' name = 'post' value = 'Post'></td></tr>
+            
+</table></form></div><br><hr><br>	
+            
+            
  <?php
 	$conn->close();
 ?>
