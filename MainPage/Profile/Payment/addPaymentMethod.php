91b89b494039336d979e7bd83ff2dc46acc5a9ce
axcel12 <axcel.duarte_espinoza@uconn.edu>
2015-04-28 23:19:42 -0400

    Payment Methods recognized and showing correct info according to selection.

--- MainPage/Profile/Payment/addPaymentMethod.php
+++ MainPage/Profile/Payment/addPaymentMethod.php
@@ -1,3 +1,11 @@
 <?php
-$myvalue=$_POST['myvalue'];
+	// Do action for Finish
+	// add payment method info to its correct payment method. Show alert confirming payment and successful creation of new job and go back to review jobs posted.
+	
+	
+	// Do action for Cancel
+	// delete job just posted because payment was not succesfull, go back to postNewJob.
+     if($_POST){
+        $PaymentMethod=$_POST['PaymentMethod'];
+     }
 ?>
